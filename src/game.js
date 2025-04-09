ddaebd22af99b5df1279ee4bcf34e13ee20730dd
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2025-04-09 12:43:27 +0200

    Built site for gh-pages

--- src/game.js
+++ src/game.js
@@ -111,7 +111,7 @@ const Game = {
             const focus =
               Math.abs(this.images.depth.pixels[indexSrc] - this.depth) < 15;
             let focalDistance =
-              Math.abs(this.images.depth.pixels[indexSrc] - depth) / 10;
+              Math.abs(this.images.depth.pixels[indexSrc] - depth) / 15;
             // Clamp the value between 0 and 1
             focalDistance = constrain(focalDistance, 0, 1);
             // focalDistance = focalDistance * focalDistance;
